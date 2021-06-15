1c767020fa00f927416d1897f4965c79f2c3b21b
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-06-15 14:27:38 +0300

    re submit

--- README.md
+++ README.md
@@ -4,19 +4,24 @@
 **Version**: 1.0.0
 
 ## Overview
+
 As a user I want to be able to view location in the map for the places before I visit it.
 
 ## Getting Started
+
 enter the correct city name then press 'Explore!'
 
 ## Architecture
+
 We use Bootstrap library, React.
 
 ## Change Log
+
 06-13-2021 4:30pm - Set up your React repository & API keys. --> 6:00pm
 06-13-2021 6:00pm - Locations. --> 6:45pm
 06-13-2021 8:30pm - Map. --> 9:00pm
 06-13-2021 9:30pm - Errors. --> 10:30pm
 
 ## Credit and Collaborations
+
 https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#client_error_responses
--- src/Components/MapAndData.js
+++ src/Components/MapAndData.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react'
-import Card from 'react-bootstrap/Card';
-import Image from 'react-bootstrap/Image';
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
 
 export class MapAndData extends Component {
   render() {
     return (
       <Card
-         bg="secondary"
-         text="white"
-         className="text-center p-3"
-         style={{ width: "30%", height: "100%" , margin: 'Auto', marginBottom: '30px'}}>
-            <Image style={{ width: "100%" }} src={`https://maps.locationiq.com/v3/staticmap?key=pk.d36871f015649f915282f374cff76628&q&center=${this.props.cityInfo.lat},${this.props.cityInfo.lon}&zoom=15`} rounded  />
-         <Card.Body>
-           <Card.Title>{this.props.cityInfo.display_name}</Card.Title>
-           <Card.Text>
-             <p>Latitude: {this.props.cityInfo.lat}</p>
-             <p>Longitude: {this.props.cityInfo.lon}</p>
-           </Card.Text>
+        bg="secondary"
+        text="white"
+        className="text-center p-3"
+        style={{ width: "30%", height: "100%", margin: "Auto", marginBottom: "30px" }}
+      >
+        <Image
+          style={{ width: "100%" }}
+          src={`https://maps.locationiq.com/v3/staticmap?key=pk.d36871f015649f915282f374cff76628&q&center=${this.props.cityInfo.lat},${this.props.cityInfo.lon}&zoom=15`}
+          rounded
+        />
+        <Card.Body>
+          <Card.Title>{this.props.cityInfo.display_name}</Card.Title>
+          <Card.Text>
+            <p>Latitude: {this.props.cityInfo.lat}</p>
+            <p>Longitude: {this.props.cityInfo.lon}</p>
+          </Card.Text>
         </Card.Body>
       </Card>
-    )
+    );
   }
 }
 
-export default MapAndData
+export default MapAndData;
