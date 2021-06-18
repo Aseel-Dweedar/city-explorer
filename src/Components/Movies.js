e34fe8d7c1b7b1da033ed1e9f442edac1c6058b8
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-06-18 15:36:44 +0300

    fix movies

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,31 +1,38 @@
 import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
+import CardColumns from "react-bootstrap/CardColumns";
 
 export class Movies extends Component {
   render() {
     return (
-      <Card
-        bg="secondary"
-        text="white"
-        className="text-center p-3"
-        style={{ margin: "50px", width: "30%", height: "750px", display: "inline-block", overflow: "auto" }}
-      >
-        <Card.Title> {this.props.moviesData.title} </Card.Title>
-        <Card.Img
-          style={{ maxHeight: "350px" }}
-          variant="top"
-          src={`https://image.tmdb.org/t/p/w500${this.props.moviesData.image_url}`}
-          alt={this.props.title}
-          onClick={this.renderModel}
-        />
-        <Card.Body>
-          <Card.Text> Total vote ❤️ = {this.props.moviesData.total_votes} </Card.Text>
-          <Card.Text> Vote average: {this.props.moviesData.average_vote} </Card.Text>
-          <Card.Text>Popularity: {this.props.moviesData.popularity} </Card.Text>
-          <Card.Text> Released date: {this.props.moviesData.released_on} </Card.Text>
-          <Card.Text> Review: {this.props.moviesData.overview} </Card.Text>
-        </Card.Body>
-      </Card>
+      <CardColumns style={{ display: "flex", flexWrap: "wrap" }}>
+        {this.props.moviesData.map((movie) => {
+          return (
+            <Card
+              bg="secondary"
+              text="white"
+              className="text-center p-3"
+              style={{ display: "inline-block", width: "400px", height: "500px", margin: "20px", overflow: "auto" }}
+            >
+              <Card.Title> {movie.title} </Card.Title>
+              <Card.Img
+                style={{ maxHeight: "350px" }}
+                variant="top"
+                src={`https://image.tmdb.org/t/p/w500${movie.image_url}`}
+                alt={this.props.title}
+                onClick={this.renderModel}
+              />
+              <Card.Body>
+                <Card.Text> Total vote ❤️ = {movie.total_votes} </Card.Text>
+                <Card.Text> Vote average: {movie.average_vote} </Card.Text>
+                <Card.Text>Popularity: {movie.popularity} </Card.Text>
+                <Card.Text> Released date: {movie.released_on} </Card.Text>
+                <Card.Text> Review: {movie.overview} </Card.Text>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </CardColumns>
     );
   }
 }
